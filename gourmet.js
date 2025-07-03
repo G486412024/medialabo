356cbf0c2666efb6524aacef28af6b4c11522016
G486412024 <248641@st.takushoku-u.ac.jp>
2025-07-03 19:40:15 +0900

    k

--- gourmet.js
+++ gourmet.js
@@ -26,33 +26,39 @@ function print(data) {
 
 }
 
-
 b = document.querySelector('button#genre');
 b.addEventListener('click', greeting);
 function greeting() {
   let i = document.querySelector('input[name="ran"]');
   ran = i.value;
   i.getAttribute('name');
-  let kaitou = '10件がヒットしました';
-  p = document.querySelector('p#hit');
-  p.textContent = kaitou;
   console.log('検索キー:' + ran);
 }
 
+
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
+
+  let shops = data.results.shop;
+
+  // ヒット件数の表示
+  let kaitou = shops.length + '件がヒットしました';
+  let p = document.querySelector('p#hit');
+  p.textContent = kaitou;
+
+
 
   let v = document.createElement('span');
   let y = document.querySelector('p#hit');;
   y.insertAdjacentElement('beforeend', v);
 
-  
+
   if (v != null) {
     v.remove();
   }
 
 
-  
+
   let span = document.createElement('span');
   y.appendChild(span);
 
@@ -62,7 +68,6 @@ function printDom(data) {
   let u = document.createElement('ul');
   t.appendChild(u);
 
-  let shops = data.results.shop;
 
   for (let i = 0; i < shops.length; i++) {
     let shop = shops[i];
@@ -112,7 +117,55 @@ d.addEventListener('click', sendRequest);
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
-  let url = 'https://www.nishita-lab.org/web-contents/jsons/hotpepper/G001.json';
+
+
+  let i = document.querySelector('input[name="ran"]');
+  let kensaku = i.value;  // 入力値を取得
+  let code;
+
+  // 入力されたジャンル名をGコードに変換
+  if (kensaku === '居酒屋') {
+    code = 'G001';
+  } else if (kensaku === 'ダイニングバー・バル') {
+    code = 'G002';
+  } else if (kensaku === '創作料理') {
+    code = 'G003';
+  } else if (kensaku === '和食') {
+    code = 'G004';
+  } else if (kensaku === '洋食') {
+    code = 'G005';
+  } else if (kensaku === 'イタリアン・フレンチ') {
+    code = 'G006';
+  } else if (kensaku === '中華') {
+    code = 'G007';
+  } else if (kensaku === '焼肉・ホルモン') {
+    code = 'G008';
+  } else if (kensaku === 'アジア・エスニック料理') {
+    code = 'G009';
+  } else if (kensaku === '各国料理') {
+    code = 'G010';
+  } else if (kensaku === 'カラオケ・パーティ') {
+    code = 'G011';
+  } else if (kensaku === 'バー・カクテル') {
+    code = 'G012';
+  } else if (kensaku === 'ラーメン') {
+    code = 'G013';
+  } else if (kensaku === 'カフェ・スイーツ') {
+    code = 'G014';
+  } else if (kensaku === 'その他グルメ') {
+    code = 'G015';
+  } else if (kensaku === 'お好み焼き・もんじゃ') {
+    code = 'G016';
+  } else if (kensaku === '韓国料理') {
+    code = 'G017';
+  } else {
+    let p = document.querySelector('p#hit');
+  p.textContent = '対応する結果がありません';
+  return;
+  }
+
+
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/hotpepper/' + code + '.json';
 
   // 通信開始
   axios.get(url)
